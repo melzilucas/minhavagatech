63ce6c9886615cacf12bca668b2e3ecf97cdd871
melzilucas <melzi.lucas@outlook.com>
2023-03-25 13:30:19 -0300

    Add news projects

---
+++ python-projects/generator-cpf.py
@@ -0,0 +1,37 @@
+"""
+gerador de cpf
+"""
+import random
+contador_gerador = 0
+
+for _ in range(1000):
+    nove_digitos = ''
+    for i in range(9):
+        nove_digitos += str(random.randint(0, 9))
+
+    contador_regressivo_1 = 10
+
+    resultado_digito_1 = 0
+    for digito in nove_digitos:
+        resultado_digito_1 += int(digito) * contador_regressivo_1
+        contador_regressivo_1 -= 1
+    digito_1 = (resultado_digito_1 * 10) % 11
+    digito_1 = digito_1 if digito_1 <= 9 else 0
+
+    dez_digitos = nove_digitos + str(digito_1)
+    contador_regressivo_2 = 11
+
+    resultado_digito_2 = 0
+    for digito in dez_digitos:
+        resultado_digito_2 += int(digito) * contador_regressivo_2
+        contador_regressivo_2 -= 1
+    digito_2 = (resultado_digito_2 * 10) % 11
+    digito_2 = digito_2 if digito_2 <= 9 else 0
+
+    contador_gerador +=1
+
+    cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+    print(f'O seu CPF {contador_gerador} comleto é: ', cpf_gerado_pelo_calculo)
+
+print(f'Gerados {contador_gerador} CPF ao total')
---
+++ python-projects/jogo-perguntas.py
@@ -0,0 +1,57 @@
+# Exercício - sistema de perguntas e respostas
+
+
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    },
+]
+
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta:', pergunta['Pergunta'])
+    print()
+
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+    print()
+
+    escolha = input('Escolha uma opção: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou 👍')
+    else:
+        print('Errou ❌')
+
+    print()
+
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
