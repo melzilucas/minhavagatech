2818f592ed72cc0db3bfe7c5dcba2d2db00ea226
melzilucas <melzi.lucas@outlook.com>
2023-04-04 16:45:02 -0300

    add python youtube audio download

---
+++ python-projects/audio-youtube-download.py
@@ -0,0 +1,25 @@
+from pytube import YouTube
+from moviepy.editor import *
+
+# Link do vídeo que deseja baixar
+link = input('Qual vídeo você deseja baixar? ')
+
+# Cria uma instância do objeto YouTube
+yt = YouTube(link)
+
+print('Baixando o vídeo: ', yt.title, 'que possui', yt.views, 'visualizações')
+
+# Obtém a melhor qualidade de áudio disponível
+audio = yt.streams.filter(only_audio=True).first()
+
+# Baixa o arquivo de áudio em formato mp4
+audio_file = audio.download()
+
+# Converte o arquivo de áudio para mp3
+print('Convertendo seu vídeo em áudio')
+mp3_file = audio_file.split('.')[0] + '.mp3'
+AudioFileClip(audio_file).write_audiofile(mp3_file)
+
+# Remove o arquivo de áudio em formato mp4
+os.remove(audio_file)
+print('Finalizado com sucesso')
